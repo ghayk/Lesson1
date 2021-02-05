4bb0e20dc68a9550e0808a45d930ed7032e2b775
ghayk <haykg45@gmail.com>
2021-02-05 23:20:51 +0400

    End lrsson4

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Counter from './components/Counter'
 
 class App extends Component {
   state = {
-    addCar:[],
-    arrCard: [  {
+    empty: false,
+    arrCard: [{
       title: 'Honda',
       text: 'The Honda logo is a large “H” appeared as the brand’s badge.',
       img: 'https://www.carlogos.org/car-logos/honda-logo.png',
@@ -36,27 +36,37 @@ class App extends Component {
       imgAlt: 'Nissan',
       active: false
     },
-  ]}
+    ]
+  }
 
   Click = (index) => {
-    this.setState({arrCard: this.state.arrCard.map((acc, ind) => {
-      if (ind === index) {
-        acc.active = !acc.active
-        // console.log(`${acc.title} active - ${acc.active}`);
-      }
-      return acc
-    })})
+    this.setState({
+      arrCard: this.state.arrCard.map((acc, ind) => {
+        if (ind === index) {
+          acc.active = !acc.active
+          // console.log(`${acc.title} active - ${acc.active}`);
+        }
+        return acc
+      })
+    })
   }
-  
-  render(){
-  return (
-    <div className="App">
-      <Card arrCard={this.state.arrCard} Click={this.Click} />
-      <AddNewCar arrCard={this.state.arrCard} click={this.Click}/>
-      <Counter />
+  Close = (index) => {
+    this.setState({ arrCard: this.state.arrCard.splice(index, 1) })
+
+  }
+
+   render() {
+     let {empty} = this.state
+     this.state.arrCard.length>0?empty=false:empty=true
+    return (
+      <div className="App">
+        <AddNewCar arrCard={this.state.arrCard} click={this.Click} />
+       {empty?<h1>List is empty</h1>:<Card arrCard={this.state.arrCard} Click={this.Click} Close={this.Close}/>}
+        <Counter />
       </div>
-      );
-    }
+    );
+
   }
+}
 
 export default App;
