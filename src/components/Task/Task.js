4463b2210060eed1b9b4978d4153f0931204309b
ghayk <haykg45@gmail.com>
2021-02-20 13:15:54 +0400

    end lesson11

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faWindowClose } from '@fortawesome/free-solid-svg-icons'
 
-class Task extends Component {
-  render() {
+class Task extends PureComponent {
+  render() {   
     const { CloseTask, togleId, DellTasks, DellAll,Disabled } = this.props
     const tasks = this.props.tasks.map((item) => {
       return (
         <Col key={item._id} className="col" xs={6} sm={4} md={3} lg={2}>
-          <div className="block">
+          <div className={`block ${item.checked?'checked':''}`}>
             {item.title}
             <button disabled={Disabled()} className="btnClose" onClick={() => CloseTask(item._id)}>
               <FontAwesomeIcon icon={faWindowClose} />
