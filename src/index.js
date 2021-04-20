a2cebd71543437dad8b25d8752f0ba937be1794e
ghayk <haykg45@gmail.com>
2021-04-20 23:21:52 +0300

    end main

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './Reset.css'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-datepicker/dist/react-datepicker.css'
